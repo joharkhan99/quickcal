3859e04433885fac7d02a42973f2a167bceda8fc
single-byte <joharkhan1999@gmail.com>
2024-02-18 16:02:17 +0500

    Refactor home page layout and add calendar functionality

---
+++ lib/models/calendar.dart
@@ -0,0 +1,60 @@
+class Calendar {
+  late DateTime _currentMonth;
+  late int totalDaysInMonth;
+
+  Calendar() {
+    _currentMonth = DateTime.now();
+    totalDaysInMonth = DateTime(_currentMonth.year, _currentMonth.month + 1, 0).day;
+  }
+
+  List<Map<String, DateTime>> getDaysInMonth({DateTime? month}) {
+    month ??= _currentMonth; // If month is null, use _currentMonth
+    List<Map<String, DateTime>> daysInMonth = [];
+    DateTime firstDayOfMonth = DateTime(month.year, month.month, 1);
+    int startingDayOfWeek = firstDayOfMonth.weekday;
+
+    // Add empty days to the list
+    for (int i = 1; i < startingDayOfWeek; i++) {
+      daysInMonth.add({});
+    }
+
+    final int daysCount = DateTime(month.year, month.month + 1, 0).day;
+    for (int day = 1; day <= daysCount; day++) {
+      DateTime date = DateTime(month.year, month.month, day);
+      String dayOfWeek = getDayOfWeek(date.weekday);
+
+      daysInMonth.add({
+        dayOfWeek: date,
+      });
+    }
+
+    // print(daysInMonth);
+
+    return daysInMonth;
+  }
+
+  String getDayOfWeek(int weekday) {
+    switch (weekday) {
+      case DateTime.monday:
+        return 'Mon';
+      case DateTime.tuesday:
+        return 'Tue';
+      case DateTime.wednesday:
+        return 'Wed';
+      case DateTime.thursday:
+        return 'Thu';
+      case DateTime.friday:
+        return 'Fri';
+      case DateTime.saturday:
+        return 'Sat';
+      case DateTime.sunday:
+        return 'Sun';
+      default:
+        return '';
+    }
+  }
+
+  int getTotalExtraDays() {
+    return getDaysInMonth().where((day) => day.isEmpty).length;
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quickcal/models/calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,50 +9,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Calendar calendar = Calendar();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      drawer: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.6,
-        child: const Drawer(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.zero,
-          ),
-          backgroundColor: Colors.white,
-          child: Text('Drawer'),
-        ),
-      ),
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'QuickCal',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            // fontFamily:
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: const Padding(
-                padding: EdgeInsets.only(left: 12.0),
-                child: Icon(Icons.menu),
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'February',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
               ),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            );
-          },
+            ),
+            Text(
+              '2023',
+              style: TextStyle(
+                  // fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color.fromARGB(164, 46, 43, 45)),
+            ),
+          ],
         ),
+        backgroundColor: Colors.white.withAlpha(200),
+        elevation: 0,
         actions: [
-          IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+            tooltip: 'Search',
+          ),
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem(
+                  child: Text('Settings'),
+                ),
+                const PopupMenuItem(
+                  child: Text('About'),
+                ),
+              ];
+            },
+            tooltip: 'Menu',
+          )
         ],
       ),
-      body: const Text('Home Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+            itemCount: calendar.totalDaysInMonth + 7 + calendar.getTotalExtraDays(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 7),
+            itemBuilder: (context, index) {
+              if (index < 7) {
+                return Card(
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(calendar.getDayOfWeek(index + 1)),
+                  ),
+                );
+              } else {
+                Map<String, DateTime> dayInfo = calendar.getDaysInMonth()[index - 7];
+                // String dayOfWeek = dayInfo.isEmpty ? '' : dayInfo.keys.first;
+                int date = dayInfo.isEmpty ? 0 : dayInfo.values.first.day;
+
+                if (date == 0 || dayInfo.isEmpty) {
+                  return const Card(
+                    color: Colors.white,
+                    elevation: 0,
+                  );
+                }
+
+                return Card(
+                  color: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '$date',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        // Text(
+                        //   '${date.day}',
+                        //   style: TextStyle(color: Colors.white),
+                        // ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+            }),
+      ),
     );
   }
 }
